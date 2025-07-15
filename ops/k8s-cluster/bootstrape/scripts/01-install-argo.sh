82d9b748fd3ab70495675d7197ffd544806de2ef
Vinicius Pilan <vinipilan@gmail.com>
2025-07-15 00:39:34 -0300

    feat: add minio

--- ops/k8s-cluster/bootstrape/scripts/01-install-argo.sh
+++ ops/k8s-cluster/bootstrape/scripts/01-install-argo.sh
@@ -1,2 +1,5 @@
 kubectl apply -n argocd -f ../../../argo/install.yaml
 kubectl apply -n argocd -f ../../../argo/applications
+
+# kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+# kubectl -n argocd port-forward svc/argocd-server 8080:80
