9f3a1b005b7047f1ac55539b74f8db5334f1d555
Vinicius Pilan <vinipilan@gmail.com>
2025-07-17 23:23:11 -0300

    improvements in script

--- ops/k8s-cluster/bootstrape/scripts/01-install-argo.sh
+++ ops/k8s-cluster/bootstrape/scripts/01-install-argo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-kubectl apply -n argocd -f ../../../argo
+kubectl apply -n argocd -f ../../../argo/00-install.yaml
+sleep 120
+kubectl apply -n argocd -f ../../../argo/01-all-applications.yaml
 
 # kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 # kubectl -n argocd port-forward svc/argocd-server 8080:80
